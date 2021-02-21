e66a3992158cd78c66b0d14c9ae45ea43a9892e2
Kash <kashay@agyeman.me>
2021-02-21 07:01:21 -0700

    created manager test to check for office number, manager class, getOfficeNumber(), and mocks employee for test - Passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,8 @@
-// jest tests
+// requires/connects necessary constructor functions/modules.
+const Employee = require("../lib/Employee");
+
+// returns newly generated employee
+test("New Employee generated", () => {
+  const emp = new Employee();
+  expect (typeof(emp)).toBe("object");
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,13 @@
-// jest tests
+// requires/connects necessary constructor functions/modules.
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+// Pulls from __mocks__ folder instead
+jest.mock('../lib/Employee.js');
+
+// returns test for office number w/ fake name, employee number and email.
+test("Office Number set by constructor argument", () => {
+  const testValue = 59;
+  const emp = new Manager("Ryan", 1, "Thisisnotreal@fake.com", testValue);
+  expect(emp.officeNumber).toBe(testValue);
+});
