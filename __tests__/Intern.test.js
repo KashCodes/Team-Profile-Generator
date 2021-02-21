95278b81e0e1f17618aa0e93e58f83a5d111c302
Kash <kashay@agyeman.me>
2021-02-21 09:44:58 -0700

    createdt tests and class object for intern - successful

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,23 @@
-// jest tests
+// requires/connects necessary constructor functions/modules.
+const Intern = require("../lib/Intern");
+
+// test to ensure we capture the school from the constuctor
+test("Provide School from the constructor argument", () => {
+  const testValue = "Juliard";
+  const emp = new Intern("Ryan", 1, "Thisisnotreal@fake.com", testValue);
+  expect(emp.school).toBe(testValue);
+});
+
+// ensures the employee role of intern is asigned
+test("getRole() worked!", () => {
+  const testValue = "Intern";
+  const emp = new Intern("Ryan", 1, "Thisisnotreal@fake.com", "Juliard");
+  expect(emp.getRole()).toBe(testValue);
+});
+
+// test returns the value of the school entered
+test("Get School from getSchool()", () => {
+  const testValue = "Juliard";
+  const emp = new Intern("Ryan", 1, "Thisisnotreal@fake.com", testValue);
+  expect(emp.getSchool()).toBe(testValue);
+});
